6d68b33816a1795bb75f61c150bcc64ccbb35291
Yogita <yogita.14.cdy@gmail.com>
2024-03-14 19:42:19 +0530

    added some files

--- auraartworks/src/App.js
+++ auraartworks/src/App.js
@@ -1,10 +1,6 @@
 import React, { useEffect,useState } from 'react';
 import Navbar from './components/Navbar';
 import Homepage from './components/Homepage';
-// import About from './components/About';
-// import Features from './components/Features';
-// import Artpart from './components/Artpart';
-// import Reglogin from './components/Reglogin';
 import { Route, Routes} from "react-router-dom";
 import Gallery from './components/Gallery';
 import {auth} from "./firbase";
